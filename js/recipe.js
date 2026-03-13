a25c8945c38e77038be267d67ca797971f88da9d
SaethorMH <smhspilari@gmail.com>
2026-03-13 01:12:40 +0100

    Add navigation header and recipe display functionality
    - Implemented a header with navigation links in contact, discover, and index pages.
    - Enhanced index page with recipe links and styles.
    - Created recipe.js for fetching and displaying recipe details.
    - Updated recipeList.js to handle pagination and recipe listing.
    - Added recipe.html for displaying individual recipe details.
    - Introduced CSS styles for index and recipe pages.

---
+++ js/recipe.js
@@ -0,0 +1,41 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+const recipeURL = "https://dummyjson.com/recipes/" + id;
+
+console.log(id);
+console.log(recipeURL);
+
+const rName = document.querySelector("#rName");
+const rating = document.querySelector("#rating");
+const imgContainer = document.querySelector("#imgContainer");
+const instructions = document.querySelector("#instructions");
+const ingredients = document.querySelector("#ingredients");
+
+function getData() {
+  fetch(recipeURL).then((res) => res.json().then((data) => show(data)));
+  console.log("getter data");
+}
+
+function show(data) {
+  rName.innerHTML = data.name;
+  rating.innerHTML = data.rating + " (" + data.reviewCount + " reviews)";
+  imgContainer.innerHTML = `<img src="${data.image}" alt="Image of ${data.name}">`;
+
+  data.instructions.forEach((inst) => {
+    instructions.innerHTML += `
+    <li class="instruction">${inst}</li>
+    `;
+  });
+  let i = 0;
+  data.ingredients.forEach((ing) => {
+    i++;
+    ingredients.innerHTML += `
+    
+    <input type="checkbox" id="ing${i}">
+    <label for="ing${i}" class="ingredient">${ing}</label>
+    
+    `;
+  });
+}
+
+getData();
--- js/recipeList.js
+++ js/recipeList.js
@@ -2,25 +2,53 @@ const params = new URLSearchParams(window.location.search);
 
 const rContainer = document.querySelector("#rContainer");
 const mealTypeBox = document.querySelector("#mealType");
+const bContainer = document.querySelector("#buttonContainer");
+let skipNr = params.get("skip");
+let limit = params.get("limit");
+let total;
 
 let mealtype = params.get("meal-type");
 mealTypeBox.innerHTML = mealtype;
 let listURL = "https://dummyjson.com/recipes/";
 console.log(listURL + mealtype);
 
+if (limit == "" || limit == null) {
+  limit = 6;
+}
+
 if (mealtype != null && mealtype != "") {
-  fetch(listURL + "/meal-type/" + mealtype + "?limit=6").then((res) =>
+  fetch(listURL + "/meal-type/" + mealtype + "?limit=" + limit + "&skip=" + skipNr).then((res) =>
     res.json().then((data) => {
-      console.log(data);
+      // console.log(data);
       mealTypeBox.innerHTML = mealtype;
+      total = data.total;
+      console.log(total);
+      makeButtons();
+      pageCount();
+      showRecipes(data.recipes);
+    }),
+  );
+} else if (skipNr != null && skipNr != "") {
+  fetch(listURL + "?limit=" + limit + "&skip=" + skipNr).then((res) =>
+    res.json().then((data) => {
+      // console.log(data);
+      mealTypeBox.innerHTML = "All Recipes";
+      total = data.total;
+      console.log(total);
+      makeButtons();
+      pageCount();
       showRecipes(data.recipes);
     }),
   );
 } else {
-  fetch(listURL + "?limit=6").then((res) =>
+  fetch(listURL + "?limit=" + limit + "&skip=0").then((res) =>
     res.json().then((data) => {
-      console.log(data);
+      // console.log(data);
       mealTypeBox.innerHTML = "All Recipes";
+      total = data.total;
+      console.log(total);
+      makeButtons();
+      pageCount();
       showRecipes(data.recipes);
     }),
   );
@@ -29,12 +57,35 @@ if (mealtype != null && mealtype != "") {
 function showRecipes(recipes) {
   recipes.forEach((recipe) => {
     rContainer.innerHTML += `
-<article>
+<article><a href="recipe.html?id=${recipe.id}">
 <img src=${recipe.image} alt="Picture of ${recipe.name}">
+</a>
 <p class="rating">${recipe.rating}★ (${recipe.reviewCount} reviews)</p>
 <h2>${recipe.name}</h2>
 <p class="time">${recipe.prepTimeMinutes + recipe.cookTimeMinutes} min</p>
 </article>
 `;
   });
+}
+
+function makeButtons() {
+  if (mealtype == null || mealtype == "") {
+    bContainer.innerHTML = `
+<a class="pageButton" href="./recipeList.html?limit=${limit}&skip=${skipNr - limit}">Prev</a>
+<a class="pageButton" href="./recipeList.html?limit=${limit}&skip=${Number(skipNr) + Number(limit)}">Next</a>`;
+  } else if (skipNr > 0 && skipNr / limit + 1 != Math.ceil(total / limit)) {
+    bContainer.innerHTML = `
+<a class="pageButton" href="./recipeList.html?meal-type=${mealtype}&limit=${limit}&skip=${skipNr - limit}">Prev</a>
+<a class="pageButton" href="./recipeList.html?meal-type=${mealtype}&limit=${limit}&skip=${Number(skipNr) + Number(limit)}">Next</a>`;
+  } else if (skipNr / limit + 1 == Math.ceil(total / limit)) {
+    bContainer.innerHTML = `
+<a class="pageButton" href="./recipeList.html?meal-type=${mealtype}&limit=${limit}&skip=${skipNr - limit}">Prev</a>`;
+  } else {
+    bContainer.innerHTML = `
+    <a class="pageButton" href="./recipeList.html?meal-type=${mealtype}&limit=${limit}&skip=${Number(skipNr) + Number(limit)}">Next</a>`;
+  }
+}
+
+function pageCount() {
+  document.querySelector("#pageCounter").innerHTML = `Page ${skipNr / limit + 1} of ${Math.ceil(total / limit)}`;
 }
